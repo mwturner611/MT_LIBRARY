250fa0e6ee4a0bb6e63ab95bd0a40f8fa7de86bc
mwturner611 <mwturner611@gmail.com>
2020-12-22 14:09:02 -0600

    clean code and object and prototype

---
+++ Greetr.js
@@ -0,0 +1,22 @@
+(function (global, jQuery){
+
+    var Greetr = function(firstName, lastName, language) {
+        return new Greetr.init(firstName, lastName, language);
+    }
+
+    var supportedLangs = ['en', 'es'];
+
+    Greetr.prototype = {};
+
+    Greetr.init = function(firstName, lastName, language){
+        var self = this;
+        self.firstName = firstName || '';
+        self.lastName = lastName || '';
+        self.language = language || 'en';
+    }
+
+    Greetr.init.prototype = Greetr.prototype;
+
+    global.Greetr = global.G$ = Greetr;
+
+}(window, jQuery));
