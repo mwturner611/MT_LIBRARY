8786183560d33f01fd119e01a0cefcc48ecd59a7
mwturner611 <mwturner611@gmail.com>
2020-12-22 14:26:53 -0600

    methods added to prototype

--- Greetr.js
+++ Greetr.js
@@ -6,7 +6,74 @@
 
     var supportedLangs = ['en', 'es'];
 
-    Greetr.prototype = {};
+    var greetings = {
+        en: 'Hello',
+        es: 'Hola'
+    };
+
+    var formalGreetings = {
+        en: 'Greetings',
+        es: 'Saludos'
+    };
+
+    var logMessages = {
+        en: 'Logged in',
+        es: 'Inicio sesion'
+    };
+
+    Greetr.prototype = {
+
+        fullName: function() {
+            return this.firstName + ' ' + this.lastName;
+        },
+
+        validate: function() {
+            if(supportedLangs.indexOf(this.language) === -1) {
+                throw "Invalid language";
+            }
+        },
+
+        greeting: function() {
+            return greetings[this.language] + ' ' + this.firstName + '!';
+        },
+
+        formalGreetings: function() {
+            return formalGreetings[this.language] + ', ' + this.fullName();
+        },
+
+        greet: function(formal) {
+            var msg;
+
+            if (formal) {
+                msg = this.formalGreetings();
+            }
+            else {
+                msg = this.greeting();
+            }
+
+            if (console) {
+                console.log(msg);
+            }
+
+            return this;
+        },
+
+        log: function() {
+            if (console) {
+                console.log(logMessages[this.language] + ': '+ this.fullName());
+            }
+
+            return this;
+        },
+
+        setLang: function(lang) {
+            this.language = lang;
+
+            this.validate();
+
+            return this;
+        }
+    };
 
     Greetr.init = function(firstName, lastName, language){
         var self = this;
